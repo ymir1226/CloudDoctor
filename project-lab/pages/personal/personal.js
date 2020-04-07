51cc78c4b5bc8e41c2bc3683d52528ecd31920ea
waveboo <1322720362@qq.com>
2020-04-07 21:15:36 +0800

    Add Chatroom V1

--- project-lab/pages/personal/personal.js
+++ project-lab/pages/personal/personal.js
@@ -1,5 +1,16 @@
 Page({
-  data:{
-    
-  }
+  data: {
+    userOrderId: null,
+  },
+  onLoad: function (options) {
+    // 应该使用success: res => {} 从后端获取 userOrderId
+    this.setData({
+      userOrderId: "a3510731313",
+    })
+  },
+  goChat: function (e) {
+    wx.navigateTo({
+      url: '/pages/room/room?id=' + this.data.userOrderId,
+    })
+  },
 })
